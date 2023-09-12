86f9880f4837b88e10dcf1c9f3bbc90308460508
webzonesoft <webzafar.shivek@gmail.com>
2023-09-12 14:14:28 +0200

    new launch

---
+++ assets/js/WebZS.js
@@ -0,0 +1,30 @@
+/*!
+=========================================================
+* WebZS Landing page
+=========================================================
+
+* Copyright: 2023 WebZS
+
+=========================================================
+
+* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+*/ 
+
+// smooth scroll
+$(document).ready(function(){
+    $(".navbar .nav-link").on('click', function(event) {
+
+        if (this.hash !== "") {
+
+            event.preventDefault();
+
+            var hash = this.hash;
+
+            $('html, body').animate({
+                scrollTop: $(hash).offset().top
+            }, 700, function(){
+                window.location.hash = hash;
+            });
+        } 
+    });
+}); 
